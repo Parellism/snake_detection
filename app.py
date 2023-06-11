15a4017edf7495e2b9647f86728f85bdf25f44c6
Parellism <136243661+Parellism@users.noreply.github.com>
2023-06-11 22:27:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,25 @@ from skimage.io import imread
 from PIL import Image
 from tensorflow.keras.models import load_model
 
+background_image_url = "https://png.pngtree.com/background/20220725/original/pngtree-seamless-pattern-with-colorful-snakes-and-leaves-picture-image_1771278.jpg"
+def set_background_image_style(url):
+    style = f"""
+    <style>
+    body {{
+        background-image: url("{url}");
+        background-size: cover;
+    }}
+    </style>
+    """
+    return style
+st.markdown(set_background_image_style(background_image_url), unsafe_allow_html=True)
 
 st.title('Snake Detection')
 st.text('Upload Image')
 
 model = load_model('model/keras_model.h5')
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+class_names = ['non-venomous', 'venomous']
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
@@ -23,7 +36,6 @@ if uploaded_file is not None:
 		img_resized = np.expand_dims(img_resized, axis=0)
 		y_out = model.predict(img_resized)
 		y_out = np.argmax(y_out, axis=1)
-		class_names = ['non-venomous', 'venomous']
 		predicted_class = class_names[y_out[0]]
 		plt.imshow(img_resized[0])
 		plt.show()
